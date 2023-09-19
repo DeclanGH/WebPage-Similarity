ab467bacc99a43caf8cda25d2b5788fd171402ed
DeclanGH <declanoonunkwo@gmail.com>
2023-09-18 23:33:47 -0400

    done with web scraping class for now

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,7 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-
 import javax.swing.*;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.regex.Pattern;
 
 public class MainClass {
     public static void main(String[] args) {
@@ -22,19 +18,17 @@ public class MainClass {
             int responseCode = connection.getResponseCode();
             
             if (responseCode == HttpURLConnection.HTTP_OK){
-                webScraper(userLink);
+                getLinkText(userLink);
             }
         } catch (IOException e) {
-            JFrame frame = new JFrame();
-            JOptionPane.showMessageDialog(frame,"The link you provided does not exist.","ERROR!",JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null,"The link you provided does not exist.","ERROR!",JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
     }
 
-    private static void webScraper(String userLink) throws IOException {
-        Document doc = Jsoup.connect(userLink).get();
-        Pattern pattern = Pattern.compile("[-/(|)^—.\"',:;!?]"); // Filtering out these characters and replacing with nothing
-        String title = doc.text().replaceAll(pattern.pattern(), "").toLowerCase();
-        System.out.println(title);
+    private static void getLinkText(String userLink) throws IOException {
+        WebScraper webscraper = new WebScraper();
+        String[] wordsInLink = webscraper.webScrape(userLink);
+        // System.out.println(Arrays.stream(wordsInLink).toList());
     }
 }
